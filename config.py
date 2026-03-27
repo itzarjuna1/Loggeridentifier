4f37ebd2de344f34aa510ba1c0be530a237fd6ee
ARJUN <pachangedinanath@gmail.com>
2026-03-28 02:51:49 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 import os
 
-API_ID = int(os.getenv("API_ID", "123456"))
-API_HASH = os.getenv("API_HASH", "your_api_hash")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "your_bot_token")
+API_ID = int(os.getenv("API_ID", "26950458"))
+API_HASH = os.getenv("API_HASH", "d818b8d530e4a9b209509815ab1b9c7c")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "")
 
 # Logger Group ID
-LOGGER_GC = int(os.getenv("LOGGER_GC", "-1001234567890"))
+LOGGER_GC = int(os.getenv("LOGGER_GC", "-1002419122093"))
